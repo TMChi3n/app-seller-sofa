b626564e7d2ce3a9375b2e712355f4a4228dd3ab
truongtranduy <tranminhchien15052003@gmail.com>
2024-04-06 22:50:13 +0700

    Commit message describing your changes

--- BackEnd/src/main/java/com/sofa/Backend/service/Impl/CartServiceImpl.java
+++ BackEnd/src/main/java/com/sofa/Backend/service/Impl/CartServiceImpl.java
@@ -1,15 +1,10 @@
 package com.sofa.Backend.service.Impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.sofa.Backend.repository.CartRepository;
 import com.sofa.Backend.service.CartService;
 
 @Service
 public class CartServiceImpl implements CartService {
-
-    @Autowired
-    private CartRepository cartRepository;
 
 }
